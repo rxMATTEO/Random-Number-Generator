32b086fcca8f027cc9cd3b6ca870bd3fa6f01043
a.volodin <erik_sundberg@mail.ru>
2023-10-19 16:41:36 +0300

    Settings page edit

--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
+import Header from "../components/Header.vue";
+import PaddingBox from "../components/PaddingBox.vue";
+import { ref } from "vue";
 
+const minMax = ref([20, 80]);
 </script>
 
 <template>
+  <Header />
+  <PaddingBox class="mt-10">
+    <div>
+      <h1>Настройки</h1>
+    </div>
 
+    <div class="mt-20 flex flex-col items-center justify-center">
+      <div class="w-1/3">
+        <div>
+          <p>Интервал значений чисел:</p>
+        </div>
+        <div class="mt-5 w-full flex place-content-between items-center gap-5">
+          <InputText :value="minMax[0]" class="block w-12" />
+          <Slider v-model="minMax" range class="w-full" />
+          <InputText :value="minMax[1]" class="block w-12" />
+        </div>
+      </div>
+    </div>
+  </PaddingBox>
 </template>
 
 <style scoped>
--- src/plugins/primeVueComponents.ts
+++ src/plugins/primeVueComponents.ts
@@ -2,8 +2,10 @@ import type {App} from "vue";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import Button from "primevue/button";
+import Slider from "primevue/slider";
+import InputText from "primevue/inputtext";
 
-const plugins = [DataTable, Column, Button];
+const plugins = [DataTable, Column, Button, Slider, InputText];
 
 export default {
   install(app: App) {
