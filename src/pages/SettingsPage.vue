c8d7d0e4d66e796a96e3be416ff314b060b69077
a.volodin <erik_sundberg@mail.ru>
2023-10-19 16:45:11 +0300

    Add frequency

--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -4,6 +4,7 @@ import PaddingBox from "../components/PaddingBox.vue";
 import { ref } from "vue";
 
 const minMax = ref([20, 80]);
+const frequency = ref(30)
 </script>
 
 <template>
@@ -16,12 +17,24 @@ const minMax = ref([20, 80]);
     <div class="mt-20 flex flex-col items-center justify-center">
       <div class="w-1/3">
         <div>
-          <p>Интервал значений чисел:</p>
+          <div>
+            <p>Интервал значений чисел:</p>
+          </div>
+          <div class="mt-5 w-full flex place-content-between items-center gap-5">
+            <InputText :value="minMax[0]" class="block w-12" />
+            <Slider v-model="minMax" range class="w-full" />
+            <InputText :value="minMax[1]" class="block w-12" />
+          </div>
         </div>
-        <div class="mt-5 w-full flex place-content-between items-center gap-5">
-          <InputText :value="minMax[0]" class="block w-12" />
-          <Slider v-model="minMax" range class="w-full" />
-          <InputText :value="minMax[1]" class="block w-12" />
+        <div class="mt-10">
+          <div>
+            <p>{{ `Частота генерации чисел: ${frequency} секунд`}}</p>
+          </div>
+          <div class="mt-5 w-full flex place-content-between items-center gap-5">
+            <p>1</p>
+            <Slider :model-value="frequency" v-model="frequency" class="w-full" />
+            <p>100</p>
+          </div>
         </div>
       </div>
     </div>
