51d4d2ba3b48b9466d8d0479b0048e8c3cc75c19
a.volodin <erik_sundberg@mail.ru>
2023-10-19 14:37:35 +0300

    Init

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="p-3 bg-blue-100 flex place-content-between text-sm">
+    <div>
+      <p>Генератор случайных чисел</p>
+    </div>
+    <div class="flex gap-5 text-blue-500">
+      <a href="#">История</a>
+      <a href="#">Настройки</a>
+      <a href="#">Любимые числа</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
