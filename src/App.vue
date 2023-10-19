cf10b379c1d0c74720ab92ff2e589ba9fbe8a6af
a.volodin <erik_sundberg@mail.ru>
2023-10-19 14:42:03 +0300

    Add random numbers table

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue";
+import RandomNumbersTable from "./components/RandomNumbersTable.vue";
 </script>
 
 <template>
   <Header />
+  <RandomNumbersTable />
 </template>
 
 <style scoped>
---
+++ src/components/RandomNumbersTable.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="mt-10 flex justify-center gap-5">
+    <div>a</div>
+    <div>b</div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
